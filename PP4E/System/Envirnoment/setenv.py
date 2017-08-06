94439650faaa736986f56b8b89381dc1cba6e1b4
Max <ozavr@me.com>
2017-08-06 20:57:06 +0500

    Системное программирование: работа с окружением

---
+++ PP4E/System/Envirnoment/setenv.py
@@ -0,0 +1,15 @@
+import os
+
+
+print('setenvv...', end=' ')
+print(os.environ['USER'])
+
+os.environ['USER'] = 'Brian'
+os.system('python3 echoenv.py')
+
+os.environ['USER'] = 'Arthur'
+os.system('python3 echoenv.py')
+
+os.environ['USER'] = input('?')
+print(os.popen('python3 echoenv.py').read())
+
--- PP4E/System/more.py
+++ PP4E/System/more.py
@@ -4,12 +4,11 @@
 
 def more(text, numlines=15):
     lines = text.splitlines()
-    print(lines)
     while lines:
         chunk = lines[:numlines]
         lines = lines[numlines:]
         for line in chunk: print(line)
-        if lines and input('More') not in ['y', 'Y']: break
+        if lines and input('More? ') not in ['y', 'Y']: break
 
 
 if __name__ == '__main__':
