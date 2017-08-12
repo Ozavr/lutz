efcaa99eabef72289256086d64b822a51f4e25b5
Max <ozavr@me.com>
2017-08-12 21:53:13 +0500

    Системное программирование: стандартные потоки 2

--- PP4E/System/more.py
+++ PP4E/System/more.py
@@ -13,4 +13,7 @@ def more(text, numlines=15):
 
 if __name__ == '__main__':
     import sys
-    more(open(sys.argv[1]).read(), 10)
+    if len(sys.argv) == 1:  # вывести данные из stdin, если нет аргументов
+        more(sys.stdin.read())
+    else:
+        more(open(sys.argv[1]).read())
